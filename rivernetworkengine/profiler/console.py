665c580b3c445e9d25cdc6b6e2fe39732d247f4e
Matthew Reimer <matt.reimer@gmail.com>
2017-03-15 10:38:03 -0700

    successfully writing and ordering CSV file outputs

--- rivernetworkengine/profiler/console.py
+++ rivernetworkengine/profiler/console.py
@@ -1,45 +1,51 @@
 import argparse
 import sys
+from os import path
 from rivernetworkengine import Logger
-import profiler
+from profiler import Profile
 
 def console():
     # parse command line options
     parser = argparse.ArgumentParser()
     parser.add_argument('shapefile',
                         help='Path to the SHP input file',
-                        type=argparse.FileType)
-    parser.add_argument('--start_id',
-                        help='Path to the CSV output file',
-                        type=argparse.FileType)
-    parser.add_argument('--end_id',
-                        help='(Optional) ID of endppoint. Will use outflow as a default.',
-                        type=argparse.FileType)
-    parser.add_argument('--id_name',
-                        help='Name of the field to use as id. \'OBJECTID\' is used as default.',
-                        type=argparse.FileType)
-    parser.add_argument('--attrib',
-                        help='Comma-separated list of attributes to summarize.',
-                        type=argparse.FileType)
+                        type=str)
     parser.add_argument('csvoutput',
                         help='Path to the CSV output file',
                         type=str)
-    parser.add_argument('--program',
-                        default="thing",
-                        help='Path or url to the Program XML file (optional)')
+    parser.add_argument('--start_id',
+                        help='Path to the CSV output file',
+                        type=int)
+    parser.add_argument('--end_id',
+                        help='(Optional) ID of endppoint. Will use outflow as a default.',
+                        type=int)
+    parser.add_argument('--cols',
+                        help='(Optional) Comma-separated list of attributes/columns to summarize. If not provided then all will be included',
+                        type=str)
     parser.add_argument('--logfile',
                         help='Write the results of the operation to a specified logfile (optional)')
     parser.add_argument('--verbose',
                         help = 'Get more information in your logs (optional)',
                         action='store_true',
                         default=False)
+
     args = parser.parse_args()
 
     log = Logger("Program")
     log.setup(args)
 
     try:
-        profiler.main(args.shapefile.name)
+        if not path.isfile(args.shapefile):
+            raise Exception('Shapefile not found: {'.format(args.shapefile))
+        if not args.start_id:
+            raise Exception('Must include a valid --start_id parameter')
+        theProfile = Profile(args.shapefile, args.start_id, args.end_id)
+
+        includedcols = ""
+        if args.cols:
+            includedcols = args.cols
+
+        theProfile.writeCSV(args.csvoutput, includedcols)
     except AssertionError as e:
         log.error("Assertion Error", e)
         sys.exit(0)
--- rivernetworkengine/profiler/profiler.py
+++ rivernetworkengine/profiler/profiler.py
@@ -17,12 +17,15 @@ class Profile():
         :param inID:  The ID of the input network segment
         :param outID:  The ID of the output network segment (Optional) Default is none
         """
+        # TODO: Could not find because those points are in two different subnetworks. Please fix your network
+        # TODO: Could not find because stream flow was a problem. If you reverse your input and output then it works
+
         log = Logger("Main")
 
         # We need to open the shapefile first:
         log.info("Opening shapefile...")
-        shape = Shapefile(shpfile)
-
+        self.network = Shapefile(shpfile)
+        self.idfield = self.network.getIDField()
         # Parse the network
         try:
             log.info("parsing shapefile into network...")
@@ -32,55 +35,54 @@ class Profile():
             log.error("Error parsing network", e)
         log = Logger("Main")
 
-        startNode = self.findnodewithID(G, inID, shape.getIDField())
+        startNode = self.findnodewithID(G, inID, self.idfield)
 
         if not startNode:
             raise Exception("Could not find start ID: {} in network.".format(inID))
 
         if outID:
-            endNode = self.findnodewithID(G, outID, shape.getIDField())
+            endNode = self.findnodewithID(G, outID, self.idfield)
             if not endNode:
                 raise Exception("Could not find end ID: {} in network.".format(outID))
             # Make a depth-first tree from the first headwater we find
             try:
                 shortestpath = nx.shortest_path(G, source=startNode[0], target=endNode[1])
-                path_edges = zip(shortestpath, shortestpath[1:])
+                self.path_edges = zip(shortestpath, shortestpath[1:])
             except Exception, e:
                 log.error("Path not found between these two points with id: '{}' and '{}'".format(inID, outID))
                 raise e
         else:
             try:
-                path_edges = list(nx.dfs_edges(G, startNode[0]))
+                self.path_edges = list(nx.dfs_edges(G, startNode[0]))
             except Exception, e:
                 log.error("Path not found between input point with ID: {} and outflow point".format(inID))
 
 
-        outList = []
+        self.attr = []
+        log.info('Calculating lengths...')
         cummulativelength = 0
-        for edge in path_edges:
+        for edge in self.path_edges:
             # Get the ID for this edge
-            shapeID = G.get_edge_data(*edge)[shape.getIDField()]
+            shapeID = G.get_edge_data(*edge)[self.idfield]
 
-            shapelyObj = shape.featureToShapely(shapeID)
+            # We do this later so that we don't have to convert every shape.
+            shapelyObj = self.network.featureToShapely(shapeID)
+
             # NetworkX stores the fields in attribute tables but we retrieve them from the shapefile directly
             # to remain consistent
-            attrDic = shapelyObj['fields']
+            attrField = shapelyObj['fields']
 
-            # Calculate length and cummulative length
-            attrDic['ProfileCalculatedLength'] = shapelyObj['geometry'].length
-            cummulativelength += attrDic['ProfileCalculatedLength']
-            attrDic['ProfileCummulativeLength'] = cummulativelength
-            outList.append(attrDic)
+            attrCalc = {}
+            attrCalc['ProfileCalculatedLength'] = shapelyObj['geometry'].length
+            cummulativelength += attrCalc['ProfileCalculatedLength']
+            attrCalc['ProfileCummulativeLength'] = cummulativelength
 
-
-        # Now let's calculate us some distances
-
-        return outList
-
-        # Could not find because those points are in two different subnetworks. Please fix your network
-        # Could not find because stream flow was a problem. If you reverse your input and output then it works
-
-
+            # Calculate length and cumulative length
+            self.attr.append({
+                'shpfields': attrField,
+                'calculated': attrCalc
+            })
+        log.info('Pathfinding complete. Found a path with {} segments'.format(len(self.attr)))
 
 
     def findnodewithID(self, G, id, idField):
@@ -92,34 +94,77 @@ class Profile():
         Logger("FindWithID")
         return next(iter([e for e in G.edges_iter() if G.get_edge_data(*e)[idField] == id]), None)
 
-    def summarizeAttrib(self, G, attrlist, shortpath, csv):
-        """
-        Here's us summing all of the attrib ('Shape_Leng' in this case)
-        :param path_edges:
-        :param GG:
-        :return:
-        """
-        Logger("SummAttrib")
-        x = []
-        counter = 0
-        for pe in shortpath:
-            counter += G.get_edge_data(*pe)[attrlist]
-            x.append(counter)
-        y = [(t[0][1] + t[0][0]) for t in shortpath]
-        writeCSV(y, csv)
 
-
-    def writeCSV(self, outdict, filename):
+    def writeCSV(self, filename, colstr = ""):
         """
         Separate out the writer so we can test without writing files
         :param outdict:
         :param csv:
         :return:
         """
+        log = Logger("CSV Writer")
+        results = []
+        log.info("Writing CSV file")
+        if len(self.attr) == 0:
+            log.error("WARNING: No rows to write to CSV. Nothing done")
+            return
 
-        with open(filename, 'wb') as csv_file:
+        # Make a subset dictionary
+        includedShpCols = []
+        if len(colstr) > 0:
+            inputDesiredCols = colstr.split(',')
+            for col in inputDesiredCols:
+                if col not in results[0]:
+                    log.error("WARNING: Could not find column '{}' in shapefile".format(col))
+                else:
+                    includedShpCols.append(col)
+        else:
+            includedShpCols = self.attr[0]['shpfields'].keys()
 
-            writer = csv.writer(csv_file)
-            writer.we
-            for key, value in outdict.items():
-               writer.writerow([key, value])
+        # Now just pull out the columns we need
+        for node in self.attr:
+            csvDict = {}
+
+            # The ID field is not optional
+            csvDict[self.idfield] = node['shpfields'][self.idfield]
+
+            # Only some of the fields get included
+            for key, val in node['shpfields'].iteritems():
+                if key in includedShpCols:
+                    csvDict[key] = val
+            # Everything calculated gets included
+            for key, val in node['calculated'].iteritems():
+                csvDict[key] = val
+            results.append(csvDict)
+
+        with open(filename, 'wb') as filename:
+            keys = results[0].keys()
+
+            # pyt the keys in order
+            def colSort(a, b):
+                # idfield should bubble up
+                item = self.attr[0]
+                if a == self.idfield:
+                    return -1
+                if b == self.idfield:
+                    return 1
+                # put shpfields ahead of calc fields
+                if (a in item['shpfields'] and b in item['calculated']):
+                    return -1
+                if (a in item['calculated'] and b in item['shpfields']):
+                    return 1
+                # Sort everything else alphabetically
+                if (a in item['shpfields'] and b in item['shpfields']) or (a in item['calculated'] and b in item['calculated']):
+                    if a.lower() > b.lower():
+                        return 1
+                    elif a.lower() < b.lower():
+                        return -1
+                    else:
+                        return 0
+
+            keys.sort(colSort)
+
+
+            writer = csv.DictWriter(filename, keys)
+            writer.writeheader()
+            writer.writerows(results)
